606f0aebe0adf9cad3147822f79be8e401c8d432
florian <floriannberisha@hotmail.com>
2025-03-30 15:48:27 +0200

    changed cards in Hero section + some small changes

--- src/pages/AboutUs.tsx
+++ src/pages/AboutUs.tsx
@@ -4,6 +4,7 @@ import FirstSection from "../components/AboutUs/FirstSection";
 import WelcomeToOurCompany from "../components/WelcomeToOurCompany";
 import Cta from "../components/Cta";
 import Faq from "../components/Faq";
+import GradientCard from "../components/GradientCard";
 
 const AboutUs: React.FC = () => {
   return (
@@ -13,6 +14,9 @@ const AboutUs: React.FC = () => {
       <Cta />
       <WelcomeToOurCompany />
       <Faq />
+      <div className="px-[10%] py-32">
+        <GradientCard title="We specialises in the following." />
+      </div>
     </div>
   );
 };
